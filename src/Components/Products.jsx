d1da13f62afce0345ff00027c9a128b410fa5562
Shital Unde <pradeep@rsmart.fi>
2024-03-17 16:54:14 +0530

    pagination

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,19 +1,39 @@
 import { useContext } from "react";
 import { ProductsContext } from "../Context/ProductsContext";
 import Product from "./Product";
+CustomPagination;
 import { Row, Col } from "react-bootstrap";
+import CustomPagination from "./CustomPagination";
 
 export default function Products() {
-  const {products} = useContext(ProductsContext);
+  const { products } = useContext(ProductsContext);
 
   return (
-    <Row xs={1} md={2} lg={3} xl={4} className="g-4">
-      {products &&
-        products.map((el, index) => (
-          <Col key={index}>
-            <Product product={el} parentIndex={index} />
-          </Col>
-        ))}
-    </Row>
+    <>
+      {products && (
+        <>
+        <Row xs={1} md={2} lg={3} xl={4} className="g-4">
+          {products.map((el, index) => (
+            <Col key={index}>
+              <Product product={el} parentIndex={index} />
+            </Col>
+          ))}
+        </Row>
+
+        <CustomPagination/>
+        </>
+      )}
+    </>
   );
 }
+
+// {
+//   <Row xs={1} md={2} lg={3} xl={4} className="g-4">
+//     {products &&
+//       products.map((el, index) => (
+//         <Col key={index}>
+//           <Product product={el} parentIndex={index} />
+//         </Col>
+//       ))}
+//   </Row>;
+// }
